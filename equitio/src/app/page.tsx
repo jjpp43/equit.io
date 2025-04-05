0b99e0fe0f6aa2e55c6dc9b1fc82c36583756698
Jazzy <jparkdev63@gmail.com>
2025-04-05 13:36:33 -0400

    landing page done

--- equitio/src/app/page.tsx
+++ equitio/src/app/page.tsx
@@ -1,11 +1,15 @@
-import { Button } from "@/components/ui/button";
+"use client";
+
+import { Card } from "@/components/ui/card";
+import Image from "next/image";
+import { CircleChevronRight } from "lucide-react";
 
 export default function Home() {
   return (
     <>
-      <div className="h-screen max-w-screen p-4">
+      <div className="flex flex-col items-center h-screen max-w-screen p-4">
         {/* TITLE FOR LANDING PAGE */}
-        <div className="flex flex-col items-center gap-4 py-16 border-2">
+        <div className="flex flex-col items-center gap-4 py-16">
           <h1 className="flex flex-col text-4xl font-bold gap-2">
             <span className="bg-orange-200 text-orange-800 px-2 py-1 rounded-md">
               Read Better
@@ -22,10 +26,26 @@ export default function Home() {
           </h2>
         </div>
         {/* START BUTTON */}
-        <div>
-          <Button className="">
-            <div className="">Start</div>
-          </Button>
+        <div className="">
+          <Card className="w-80 flex flex-col items-center py-4 px-4">
+            <Image
+              className="rounded-lg"
+              src="/card.png"
+              width={300}
+              height={200}
+              alt=""
+            ></Image>
+
+            <div className="flex flex-row items-center px-2 h-full w-full border-t pt-4">
+              <div className="px-2 text-sm ">
+                {" "}
+                Convert text into dyslexia-friendly font
+              </div>
+              <div>
+                <CircleChevronRight />
+              </div>
+            </div>
+          </Card>
         </div>
       </div>
     </>
